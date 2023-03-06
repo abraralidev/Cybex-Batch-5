09e1802671f2855d3db550cd710fbedd02b81dd3
Abrar Ali <91776475+abraralidev@users.noreply.github.com>
2023-03-06 17:40:11 +0500

    comments added

--- class_6/lib/screens/home.dart
+++ class_6/lib/screens/home.dart
@@ -11,12 +11,23 @@ class Home extends StatelessWidget {
         ),
         body: Column(
           children: [
+            
+// here we call Expanded widget for responsiveness            
             Expanded(
                 child: Row(children: [
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.all(10.0),
                   color: Colors.red,
+// how to call assets image 
+// step 1: make a folder outside from lib with the name of assets
+// step 2: put the downloaded image in the assets folder
+// step 3: open pubspec.yaml file and remove the # from asset then the image line replace with -assets/
+// step 4: now call your image like this given below
+// tips if everything is correct but the image does't show then run these commends in ternimal
+                  // 1. flutter clean
+                  // 2. flutter pub get
+                  
                   child: Image.asset(
                     'assets/wallpaper.jpg',
                     fit: BoxFit.cover,
