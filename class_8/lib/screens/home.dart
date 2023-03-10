25d8a0c66f6a243f72df2e3246a4821ad8f50f8b
Abrar Ali <91776475+abraralidev@users.noreply.github.com>
2023-03-10 23:29:56 +0500

    Update home.dart

--- class_8/lib/screens/home.dart
+++ class_8/lib/screens/home.dart
@@ -14,7 +14,7 @@ class Home extends StatelessWidget {
               child: CircleAvatar(
                 radius: 40,
                 backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                    'https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
               ),
             ),
             ListTile(
