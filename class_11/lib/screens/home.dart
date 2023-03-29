231eb1a1341a1d0f42c2176cbaeb1b19eda4b9f8
abraralidev <abraraliiofficial@gmail.com>
2023-03-29 14:05:13 +0500

    class 11

--- class_11/lib/screens/home.dart
+++ class_11/lib/screens/home.dart
@@ -1,11 +1,26 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-   Home({super.key});
+  Home({super.key});
   var productname = [
-"saniunxia",
-"sanixia",
-"sanixia",
+    "Laptop",
+    "Keyboard",
+    "Mobile",
+    "Mouse",
+  ];
+  var priceproduct = [
+    "20000",
+    "3000",
+    "50000",
+    "1000",
+  ];
+  var imagelist = [
+    "https://images.unsplash.com/photo-1679939099475-384a2ff77e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    "https://images.unsplash.com/photo-1679939099475-384a2ff77e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    "https://images.unsplash.com/photo-1679939099475-384a2ff77e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    "https://images.unsplash.com/photo-1679939099475-384a2ff77e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
   ];
   @override
   Widget build(BuildContext context) {
@@ -19,10 +34,57 @@ class Home extends StatelessWidget {
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemCount: 4,
+        itemCount: productname.length,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: Colors.black54,
+          return Card(
+            elevation: 10,
+            child: Container(
+              // width: 200,
+              // height: 250,
+              color: Colors.black54,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    imagelist[index],
+                    width: 100,
+                    height: 100,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    productname[index],
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text(
+                        'Price',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '\$ ${priceproduct[index]}',
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
